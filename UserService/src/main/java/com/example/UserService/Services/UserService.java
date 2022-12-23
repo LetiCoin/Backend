a9c4a007547937c66741348cfddb68807e82fbe3
imserega <sergei1420011@gmail.com>
2022-12-23 04:50:38 +0300

    Add UserService

---
+++ UserService/src/main/java/com/example/UserService/Services/UserService.java
@@ -0,0 +1,16 @@
+package com.example.UserService.Services;
+
+import com.example.UserService.Models.User;
+import com.example.UserService.Models.UserCourseDto;
+
+import java.util.List;
+
+
+public interface UserService {
+    User register(User user);
+    User findByUsername(String username);
+    User findById(Long id);
+    UserCourseDto addCourse(Long courseId, Long userId);
+
+//    List<UserCourseDto> getUserCourses(Long userId);
+}
---
+++ UserService/src/main/java/com/example/UserService/mq/RabbitListener.java
@@ -0,0 +1,31 @@
+package com.example.UserService.mq;
+
+import com.example.UserService.Models.User;
+import com.example.UserService.Models.UserRegDto;
+import com.example.UserService.Services.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitListener {
+    private ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+    @Autowired
+    private UserService userService;
+
+    @org.springframework.amqp.rabbit.annotation.RabbitListener(queues = "UserReg")
+    public void authentication(String msg) {
+        try{
+            UserRegDto newUser = mapper.readValue(msg, UserRegDto.class);
+            User user = new User(newUser);
+            userService.register(user);
+        }catch (JsonProcessingException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
